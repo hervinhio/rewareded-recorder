339a7bdc90e436a3b58d1c20f510438ce234010c
Hervé Mutombo <herve@herocoders.com>
2024-09-04 10:35:34 +0100

    #457 | Convert notifications operations to MongoDb

---
+++ server/entities/notification.go
@@ -0,0 +1,16 @@
+package entities
+
+import "time"
+
+type Notification struct {
+  Id          string    `json:"id" bson:"_id,omitempty"`
+  SubjectId   string    `json:"subjectId" bson:"subjectId,omitempty"`
+  Subject     Publisher `json:"subject" bson:"subject,omitempty"`
+  SubjectType string    `json:"subjectType" bson:"subjectType,omitempty"`
+  AuthorId    string    `json:"authorId" bson:"authorId,omitempty"`
+  Author      User      `json:"author" bson:"author,omitempty"`
+  Date        time.Time `json:"date" bson:"date,omitempty"`
+  Type        int       `json:"type" bson:"type,omitempty"`
+  Unread      bool      `json:"unread" bson:"unread,omitempty"`
+  RealmId     string    `json:"realmId" bson:"realmId,omitempty"`
+}
