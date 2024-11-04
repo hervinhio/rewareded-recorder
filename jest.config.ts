17b8a7e13dd8c9ef58b1f79618293465e1111dbe
Hervé Mutombo <herve@herocoders.com>
2024-11-04 12:29:38 +0100

    #548 | Fixed failing unit tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,13 @@
-const { getJestProjects } = require('@nrwl/jest');
-import { TextEncoder, TextDecoder } from 'util'
-global.TextEncoder = TextEncoder
-// @ts-expect-error
-global.TextDecoder = TextDecoder
+const {getJestProjects} = require('@nrwl/jest');
+import {TextDecoder, TextEncoder} from 'util';
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 
 export default {
   projects: getJestProjects(),
   codeCoverage: true,
+  testEnvironment: 'jsdom',
+  setupFiles: ['<rootDir>/setup.jest.ts'],
   coverageReporters: ['lcov, json'],
   coverageDirectory: './coverage',
 };
